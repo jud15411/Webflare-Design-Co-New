e71ebfa599461771a56a5526a093c900fec4f0b2
jud15411 <117609861+jud15411@users.noreply.github.com>
2025-07-21 18:05:42 -0400

    Update taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+const auth = require('../middleware/authMiddleware');
 const Task = require('../models/Task');
 const Project = require('../models/Project'); // Needed for some checks
 
